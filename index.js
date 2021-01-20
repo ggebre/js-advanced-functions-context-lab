71cd1fb4d1b57c913fd37ee18d26635a19a6f3aa
Getu Gebre <getu.gebre.gg@gmail.com>
2021-01-20 13:13:51 -0600

    commit tree d5c48b1938d46b305a5cd9f981b2dda2c1fe86e8

--- index.js
+++ index.js
@@ -19,4 +19,69 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+} 
+
+function createEmployeeRecord([firstName, familyName, title, payPerHour]){
+    const employeeRecord = {
+        firstName, familyName, title, payPerHour, timeInEvents : [], timeOutEvents : []
+    }
+    return employeeRecord
 }
+function createEmployeeRecords(employeeRecords){
+    return employeeRecords.map(employeeRecord => createEmployeeRecord(employeeRecord))
+}
+
+
+function createTimeInEvent(dateStamp){
+   
+    const [date, time] = dateStamp.split(" ")
+   
+    this.timeInEvents.push({
+        type : "TimeIn",
+        hour : parseInt(time), 
+        date
+        
+    }) 
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    const [date, time] = dateStamp.split(" ")
+    
+    this.timeOutEvents.push({
+        type : "TimeOut",
+        hour : parseInt(time), 
+        date, 
+    }) 
+    return this
+} 
+
+function hoursWorkedOnDate(date){
+    const timeIn = this.timeInEvents.find(timeInEvent => timeInEvent.date == date).hour
+    const timeOut = this.timeOutEvents.find(timeOutEvent => timeOutEvent.date == date).hour
+    return (timeOut - timeIn)/100 
+} 
+
+function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date) 
+    const payPerHour = this.payPerHour 
+    return hoursWorked * payPerHour
+} 
+
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find(employeeRecord => employeeRecord.firstName == firstName)
+}
+
+function calculatePayroll(employeeRecords){
+    
+    let payroll = 0;
+    employeeRecords.forEach(employeeRecord => 
+        {
+            payroll += allWagesFor.call(employeeRecord)
+        })
+    // return srcArray.reduce((momo, e) => momo + allWagesFor.call(this), 0) 
+    return payroll 
+}
+
+
